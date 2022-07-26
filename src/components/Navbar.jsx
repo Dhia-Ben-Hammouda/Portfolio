271be8eb3428096f5e35dda98b69accd77002d59
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-07-26 19:53:15 +0100

    more work on ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,27 @@ const Navbar = ()=>{
       <nav>
         <FaReact className="icon"/>
 
-        <ul>
+        <ul className="desktop-list">
           <a href="#home">Home</a>
           <a href="#services">Services</a>
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </ul>
+
+        <ul className="mobile-list">
+          <a href="#home">Home</a>
+          <a href="#services">Services</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </ul>
+
+        <div className="bars">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </nav>
     </>
   )

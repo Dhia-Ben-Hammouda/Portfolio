e2bb7be8360ca70a7e480c6cda122d4915674c29
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-07-27 02:22:12 +0100

    more work on responsiveness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,52 +13,6 @@ const Skills = ()=>{
           <h2>technologies i've used</h2>
         </div>
         <div className="wrapper">
-          <div className="skill">
-            <FaHtml5 style={{color:"#F25320"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaCss3Alt style={{color:"#1598D3"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaJs style={{color:"#E4D04B"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaSass style={{color:"#C45F92"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaReact style={{color:"#5CCFEE"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <SiTailwindcss style={{color:"#07ADCA"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <SiMongodb style={{color:"#489F46"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaNodeJs style={{color:"#4B943B"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <FaGitAlt style={{color:"#E44D30"}} className="icon"/>
-          </div>
-          <div className="skill mysql">
-            <img alt="" src={mysql}/>
-          </div>
-          <div className="skill">
-            <SiMaterialui style={{color:"#00A7F2"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <SiSocketdotio style={{color:"#000"}} className="icon"/>
-          </div>
-          <div className="skill">
-            <SiRedux style={{color:"#7046B2"}} className="icon"/>
-          </div>
-          <div className="skill figma">
-            <img alt="" src={figma}/>
-          </div>
-          <div className="skill">
-            <SiTypescript style={{color:"#00A7F2"}} className="icon"/>
-          </div>
-          
 
         </div>
       </div>

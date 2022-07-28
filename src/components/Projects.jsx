e055bddc6d17afa2b5189f93f04c7e222faaa4fa
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-07-28 16:09:11 +0100

    more work on responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,28 +33,6 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="project">
-            <div className="left">
-              <img src={project1} alt="" />
-            </div>
-            <div className="right">
-              <h1>Tunisia Net Clone</h1>
-              <p>in this project i tried to clone the famous E-commerce
-                platform , Tunisia Net . </p>
-              <h3>Technologies used :</h3>
-              <div>
-                <FaHtml5 size="1.75rem" style={{margin:".5rem .2rem" , color:"F25320"}}/>
-                <FaCss3Alt size="1.75rem" style={{margin:".5rem .2rem" , color:"279FD5"}}/>
-                <FaJs size="1.75rem" style={{margin:".5rem .2rem" , color:"E4D04B"}}/>
-                <FaSass size="1.75rem" style={{margin:".5rem .2rem" , color:"CF649A"}}/>
-                <FaReact size="1.75rem" style={{margin:".5rem .2rem" , color:"5CCFEE"}}/>
-                <FaNodeJs size="1.75rem" style={{margin:".5rem .2rem" , color:"4F9640"}}/>
-                <SiMongodb size="1.75rem" style={{margin:".5rem .2rem" , color:"449E45"}}/>
-                <SiRedux size="1.75rem" style={{margin:".5rem .2rem" , color:"7248B6"}}/>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
     </section>
